113f4e71e979bcc15a3bef89179fd78f943adc46
Yurii Vasylchuk <yriy.vasilchuk@gmail.com>
2023-07-16 17:24:20 +0300

    Generate jooq classes from db in testcontainers (#13)

--- src/test/java/org/mvasylchuk/pfcc/PfccMariadbContainer.java
+++ src/test/java/org/mvasylchuk/pfcc/PfccMariadbContainer.java
@@ -3,7 +3,7 @@ package org.mvasylchuk.pfcc;
 import org.testcontainers.containers.MariaDBContainer;
 
 public class PfccMariadbContainer extends MariaDBContainer<PfccMariadbContainer> {
-    public static final String IMAGE_VERSION = "mariadb:10.4";
+    public static final String IMAGE_VERSION = "mariadb:10.5";
     public static final String USERNAME = "pfcc";
     public static final String PASSWORD = "pfcc";
     public static final String DATABASE_NAME = "pfcc";
