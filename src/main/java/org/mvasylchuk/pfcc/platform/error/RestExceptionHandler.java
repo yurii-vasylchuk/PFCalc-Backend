251073b177a379c38dd1825f96469b68cd72db1d
Yurii Vasylchuk <yriy.vasilchuk@gmail.com>
2024-05-01 18:19:06 +0300

    Improve exception handling

--- src/main/java/org/mvasylchuk/pfcc/platform/error/RestExceptionHandler.java
+++ src/main/java/org/mvasylchuk/pfcc/platform/error/RestExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.mvasylchuk.pfcc.common.dto.BaseResponse;
 import org.mvasylchuk.pfcc.platform.configuration.model.PfccAppConfigurationProperties;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Slf4j
 @RestControllerAdvice
@@ -29,20 +34,28 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(FORBIDDEN)
     protected BaseResponse<Void> handle(AccessDeniedException ignored) {
         return BaseResponse.fail("Access denied", null);
     }
 
     @ExceptionHandler(PfccException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected BaseResponse<Void> handle(PfccException e) {
+    protected ResponseEntity<BaseResponse<Void>> handle(PfccException e) {
         log.info("Invalid request", e);
-        return BaseResponse.fail("Invalid request", e.getErrorCode());
+        BaseResponse<Void> body = BaseResponse.fail("Invalid request", e.getErrorCode());
+        return ResponseEntity.status(this.translateToStatus(e))
+                .body(body);
+    }
+
+    private HttpStatusCode translateToStatus(PfccException e) {
+        return switch (e.getErrorCode()) {
+            case SECURITY -> FORBIDDEN;
+            default -> BAD_REQUEST;
+        };
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     protected BaseResponse<Void> handle(MethodArgumentNotValidException e) {
         return BaseResponse.fail(new ValidationErrorsDescriptor(e.getFieldErrors().stream()
                                                                  .map(fe -> new ValidationErrorsDescriptor.FieldValidationErrorsDescriptor(
