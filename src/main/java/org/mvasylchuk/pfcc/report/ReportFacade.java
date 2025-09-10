f5c858f88c1dd07c31b1673802981cce4053aabc
Yurii Vasylchuk <yriy.vasilchuk@gmail.com>
2025-09-10 10:26:00 +0300

    Remove commented-out Chromium command in `ReportFacade`

--- src/main/java/org/mvasylchuk/pfcc/report/ReportFacade.java
+++ src/main/java/org/mvasylchuk/pfcc/report/ReportFacade.java
@@ -72,7 +72,6 @@ public class ReportFacade {
                         outputHtmlPath.toString(),
                         "--no-pdf-header-footer"
                 });
-        // /usr/bin/chromium --headless --disable-gpu --print-to-pdf='/app/test.pdf' 'https://pfcalc.yuriivasylchuk.org.ua' --no-pdf-header-footer
         try {
             process.waitFor(conf.reports.renderTimeout.getSeconds(), TimeUnit.SECONDS);
         } catch (InterruptedException e) {
